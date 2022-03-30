cb5159160fe2f7405dd1320e68779c4126efed8c
Emre Özcan <emre.ozcan@appcent.mobi>
2022-03-30 14:12:56 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Emre Özcan on 30.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
